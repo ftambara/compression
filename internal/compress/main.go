9828e65d3c98d628f38570e5dd38333e463a2098
ftambara <98750668+ftambara@users.noreply.github.com>
2024-09-16 22:32:50 -0300

    Add huffmanTree.decode()

--- internal/compress/main.go
+++ internal/compress/main.go
@@ -1,9 +1,13 @@
 package compress
 
 import (
+	"errors"
 	"fmt"
 	"unicode"
 )
+
+// codeBufferBits defines number of bits in code processing buffers
+const codeBufferBits = 64
 
 type symbolCount struct {
 	symbol byte
@@ -31,4 +35,129 @@ func symbolCounts(symbols []byte) []symbolCount {
 		i++
 	}
 	return unique
+}
+
+type huffmanMovement int
+
+const (
+	left  huffmanMovement = 0
+	right huffmanMovement = 1
+)
+
+type errInvalidCode struct {
+	code uint64
+}
+
+func (e errInvalidCode) Error() string {
+	return fmt.Sprintf("invalid code %x", e.code)
+}
+
+var (
+	errIncompleteCode = errors.New("incomplete Huffman code")
+	errEmptyTree      = errors.New("tree has no root")
+	errEmptyNode      = errors.New("node has no children")
+)
+
+type huffmanTree struct {
+	root *huffmanNode
+}
+
+func newHuffmanTree(root *huffmanNode) huffmanTree {
+	return huffmanTree{root: root}
+}
+
+func (t huffmanTree) decode(code uint64, out []byte) (written int, used int, err error) {
+	node := t.root
+	if node == nil {
+		return 0, 0, errEmptyTree
+	}
+
+	var mask uint64 = 1 << (codeBufferBits - 1)
+
+	if (code & mask) == 0 {
+		return 0, 0, errInvalidCode{code}
+	}
+
+	// Skip the first 1 bit
+	mask >>= 1
+	used++
+
+	var bit int8
+	for {
+		if used == codeBufferBits {
+			return written, used, errIncompleteCode
+		}
+
+		if (code & mask) == 0 {
+			bit = 0
+		} else {
+			bit = 1
+		}
+		movement := huffmanMovement(bit)
+
+		node, err = node.child(movement)
+		if err != nil {
+			return written, used, err
+		}
+		mask >>= 1
+		used++
+
+		if node.isLeaf() {
+			out[written] = node.symbol
+			written++
+			node = t.root
+			if used == codeBufferBits {
+				break
+			}
+			// check for padding
+			if (code & mask) == 0 {
+				// if the rest of the code is 0, we are done
+				if code&(mask-1) == 0 {
+					return written, codeBufferBits, nil
+				}
+				return written, used, errInvalidCode{code}
+			} else {
+				// skip 1 of next code point
+				used++
+				mask >>= 1
+			}
+		}
+	}
+	return written, used, nil
+}
+
+// huffmanNode represents a node in a Huffman tree
+// Do not use this struct directly. Use newHuffmanInternalNode and newHuffmanLeaf instead,
+// as they ensure that the struct is correctly initialized.
+type huffmanNode struct {
+	left, right *huffmanNode
+	symbol      byte
+}
+
+func newHuffmanInternalNode(left, right *huffmanNode) *huffmanNode {
+	return &huffmanNode{left: left, right: right}
+}
+
+func newHuffmanLeaf(symbol byte) *huffmanNode {
+	return &huffmanNode{symbol: symbol}
+}
+
+func (n *huffmanNode) isLeaf() bool {
+	return n.left == nil && n.right == nil
+}
+
+func (n huffmanNode) child(movement huffmanMovement) (*huffmanNode, error) {
+	var node *huffmanNode
+	switch movement {
+	case left:
+		node = n.left
+	case right:
+		node = n.right
+	default:
+		return nil, fmt.Errorf("invalid movement %v", movement)
+	}
+	if node == nil {
+		return nil, errEmptyNode
+	}
+	return node, nil
 }
--- internal/compress/main_test.go
+++ internal/compress/main_test.go
@@ -5,13 +5,8 @@ import (
 	"testing"
 )
 
-func assertSliceEqual[T comparable](t *testing.T, expected, got []T) {
-	if !slices.Equal(expected, got) {
-		t.Errorf("expected %v, got %v", expected, got)
-	}
-}
-
 func assertSliceEqualSort[T comparable](t *testing.T, cmp func(a, b T) int, expected, got []T) {
+	t.Helper()
 	slices.SortFunc(expected, cmp)
 	slices.SortFunc(got, cmp)
 	if !slices.Equal(expected, got) {
@@ -51,4 +46,84 @@ func Test_symbolCounts(t *testing.T) {
 		{'s', 2},
 	}
 	assertSliceEqualSort(t, cmp, expected, counts)
+}
+
+func assertHuffmanDecoding(
+	t *testing.T,
+	tree huffmanTree,
+	code uint64,
+	expectedErr error,
+	expectedUsed int,
+	expectedDecoded []byte,
+) {
+	t.Helper()
+	out := make([]byte, codeBufferBits)
+	written, used, err := tree.decode(code, out)
+	if err != expectedErr {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if used != expectedUsed {
+		t.Errorf("expected %d, got %d", expectedUsed, used)
+	}
+	if !slices.Equal(out[:written], expectedDecoded) {
+		t.Errorf("expected %v, got %v", expectedDecoded, out[:written])
+	}
+}
+
+// alignToLeft shifts n to the left until the most significant bit is set.
+func alignToLeft(n uint64) uint64 {
+	if n == 0 {
+		return 0
+	}
+	for n&(1<<(codeBufferBits-1)) == 0 {
+		n <<= 1
+	}
+	return n
+}
+
+func Test_decodeHuffmanEmpty(t *testing.T) {
+	tree := newHuffmanTree(nil)
+	var code uint64
+	assertHuffmanDecoding(t, tree, code, errEmptyTree, 0, nil)
+
+	tree = huffmanTree{}
+	assertHuffmanDecoding(t, tree, code, errEmptyTree, 0, nil)
+
+	tree = newHuffmanTree(newHuffmanInternalNode(nil, nil))
+	assertHuffmanDecoding(t, tree, code, errInvalidCode{code}, 0, nil)
+}
+
+func Test_decodeHuffman(t *testing.T) {
+	tree := newHuffmanTree(
+		newHuffmanInternalNode(
+			newHuffmanLeaf('a'),
+			newHuffmanInternalNode(
+				newHuffmanLeaf('b'),
+				newHuffmanLeaf('c'),
+			),
+		),
+	)
+
+	code := alignToLeft(0b10)
+	assertHuffmanDecoding(t, tree, code, nil, 64, []byte{'a'})
+
+	code = alignToLeft(0b110)
+	assertHuffmanDecoding(t, tree, code, nil, 64, []byte{'b'})
+
+	code = alignToLeft(0b10111)
+	assertHuffmanDecoding(t, tree, code, nil, 64, []byte{'a', 'c'})
+
+	// Padding
+	code = alignToLeft(0b100)
+	assertHuffmanDecoding(t, tree, code, nil, 64, []byte{'a'})
+
+	// Errors
+	code = 0b010
+	assertHuffmanDecoding(t, tree, code, errInvalidCode{code}, 0, nil)
+
+	code = 0xffffffffffffffff
+	assertHuffmanDecoding(t, tree, code, errIncompleteCode, 64, []byte("ccccccccccccccccccccc"))
+
+	code = alignToLeft(0b01) >> 1
+	assertHuffmanDecoding(t, tree, code, errInvalidCode{code}, 0, nil)
 }
